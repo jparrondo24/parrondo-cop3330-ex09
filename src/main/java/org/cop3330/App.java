76e80f28bbb51493ef2e1c0381b59904a435f0a4
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:26:10 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,21 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    static final double SQUAREFEET_PER_GALLON = 350.0;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the length of the ceiling? ");
+        int length = in.nextInt();
+        System.out.print("What is the width of the ceiling? ");
+        int width = in.nextInt();
+
+        int squareFeet = length * width;
+        int numGallonsNeeded = (int) Math.ceil(squareFeet / SQUAREFEET_PER_GALLON);
+        System.out.printf("You will need to purchase %d gallons of paint to cover %d square feet.\n",
+                           numGallonsNeeded, squareFeet);
+    }
+}
